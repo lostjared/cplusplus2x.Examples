50277a1c4420ab010717201a1e49e293f623b231
Jared Bruni <jaredbruni@gmail.com>
2024-03-18 06:27:23 -0700

    pixelsort

---
+++ C++20/spaceship/Example/spaceship.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+template<typename T>
+class Color {
+public:
+    T red,green,blue;
+    auto operator<=>(const Color<T> &c) const {
+        return (color() <=> c.color());
+    }
+    size_t color() const { return red+green+blue; }
+    friend std::ostream &operator<<(std::ostream &out, const Color<T> &c) {
+        out << static_cast<int>(c.red) << "," << static_cast<int>(c.green) << "," << static_cast<int>(c.blue) << " -> " << static_cast<int>(c.color());
+        return out;
+    }
+};
+
+
+int main(int argc, char **argv) {
+    std::vector<Color<unsigned char>> colors;
+    colors.push_back({255,255,255});
+    colors.push_back({150, 50, 50});
+    colors.push_back({250, 100, 25});
+    
+    std::sort(colors.begin(), colors.end(), [](const Color<unsigned char> &c1, const Color<unsigned char> &c2) {
+        return (c2.color() < c1.color());
+    });
+    
+    std::cout << "Pixel Sorted:\n";
+    for(const auto &i : colors) {
+        std::cout << i << "\n";
+    }
+    
+    return 0;
+}
+
