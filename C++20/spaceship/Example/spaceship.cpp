7198a5f84e3eaab05dec8a3844849c9200cf16b7
Jared Bruni <jaredbruni@gmail.com>
2024-03-18 06:45:06 -0700

    use initalizer list

--- C++20/spaceship/Example/spaceship.cpp
+++ C++20/spaceship/Example/spaceship.cpp
@@ -18,20 +18,15 @@ public:
 
 
 int main(int argc, char **argv) {
-    std::vector<Color<unsigned char>> colors;
-    colors.push_back({255,255,255});
-    colors.push_back({150, 50, 50});
-    colors.push_back({250, 100, 25});
-    
+    std::vector<Color<unsigned char>> colors {{255,255,255}, {150, 50, 50}, {250, 100, 25}};
+
     std::sort(colors.begin(), colors.end(), [](const Color<unsigned char> &c1, const Color<unsigned char> &c2) {
         return (c2 < c1);
     });
-    
     std::cout << "Pixel Sorted:\n";
     for(const auto &i : colors) {
         std::cout << i << "\n";
     }
-    
     return 0;
 }
 
