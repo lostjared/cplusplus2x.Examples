7f886ab11f0b7bee50e2d108c7ab90f574c486c8
Jared Bruni <jaredbruni@gmail.com>
2024-03-20 03:42:38 -0700

    concept test

---
+++ C++20/concepts/ex4/test.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<type_traits>
+
+
+template<typename T>
+concept Object = std::is_class_v<T>;
+
+template<Object O>
+void echo(const O &o) {
+    std::cout << "Object: " << o << "\n";
+}
+
+template<typename T>
+void echo(const T &t) {
+    std::cout << "Non Object: " << t << "\n";
+}
+
+class Obj {
+public:
+    Obj() = default;
+    Obj(int valuex) : value{valuex} {}
+    int getValue() const { return value; }
+    friend std::ostream &operator<<(std::ostream &out, const Obj &o) {
+        out << "{" << o.getValue() << "} ";
+        return out;
+    }
+private:
+    int value;
+};
+
+int main() {
+    Obj object;
+    echo(object);
+    echo(10);
+    echo(25.0f);
+    return 0;
+}
