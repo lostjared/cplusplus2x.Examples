8392f5cc188112c39a4de445c6f83b68f4ea5661
Jared Bruni <jaredbruni@gmail.com>
2024-03-20 10:20:43 -0700

    updated

--- C++20/concepts/ex5/concept.cpp
+++ C++20/concepts/ex5/concept.cpp
@@ -5,12 +5,13 @@
 template<typename T>
 concept Numeric = std::is_arithmetic_v<T>;
 
+// add and return as same type as container C
 template<typename C, Numeric T1, Numeric T2>
-auto push_add(C &cont, T1 val1, T2 val2) {
+auto push_add(C &cont, T1 val1, T2 val2) -> typename C::value_type {
     cont.push_back(val1+val2);
     return val1+val2;
 }
-
+// sub and return type deduced by type1-type2
 template<typename C, typename T1, typename T2>
 requires (std::is_arithmetic_v<T1> && std::is_arithmetic_v<T2>)
 auto push_sub(C &cont, T1 type1, T2 type2) {
@@ -20,7 +21,8 @@ auto push_sub(C &cont, T1 type1, T2 type2) {
 
 int main() {
     std::vector v{0, 1, 2};
-    push_add(v, 1, 1.5); // okay narrow 1.5
+    auto num = push_add(v, 1, 1.5); // okay narrow 1.5
+    std::cout << "Narrow add on v<int>: " << num << "\n";
     //push_add(v, 1, "Two"); // ERR requires arithmetic type
     push_sub(v, 3, 2);
     auto rt = push_sub(v, 3, 3.5f); // narrow / return double without narrow
