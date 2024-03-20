b5a3fd45110a97f88cb1c938b298e58cd3a8de34
Jared Bruni <jaredbruni@gmail.com>
2024-03-20 10:31:07 -0700

    var

--- C++20/concepts/ex5/concept.cpp
+++ C++20/concepts/ex5/concept.cpp
@@ -19,6 +19,17 @@ auto push_sub(C &cont, T1 type1, T2 type2) {
     return type1-type2;
 }
 
+template<typename C, Numeric T>
+void add_int(C &cont, const T &type) {
+    cont.push_back(type);
+}
+
+template<typename C, Numeric T, Numeric... Args>
+void add_int(C &cont,const T &type, Args... args) {
+    cont.push_back(type);
+    add_int(cont, args...);
+}
+
 int main() {
     std::vector v{0, 1, 2};
     auto num = push_add(v, 1, 1.5); // okay narrow 1.5
@@ -31,6 +42,10 @@ int main() {
     std::vector v2 { 0.5, 0.1, 2.0 };
     auto rt_v = push_add(v2, 0.5, 1);
     std::cout << "add on v2<double> return type: " << rt_v << "\n";
+    
+    add_int(v, 1, 2, 3, 4, 5);
+    //add_int(v, "test"); // ERR doesn't accept char
+    
     for(auto &i : v) {
         std::cout << "index: " << i << "\n";
     }
