a8cf39b4ba777342d9e1f34a989c30007e0e67f2
Jared Bruni <jaredbruni@gmail.com>
2024-03-22 01:24:56 -0700

    Updated

--- C++20/argz/wstring_type/type.cpp
+++ C++20/argz/wstring_type/type.cpp
@@ -8,9 +8,10 @@ int main(int argc, char **argv) {
     std::locale::global(std::locale(""));
     std::wcout.imbue(std::locale());
     Argz<std::wstring> argz(argc, argv);
-    argz.addOptionSingle('h', L"Help message \u2665");
-    argz.addOptionSingle('v', L"Version Info \u263A");
-    argz.addOptionDouble('j', L"jared", L"j\u00C6red");
+    argz.addOptionSingle('h', L"Help message \u2665")
+    .addOptionSingle('v', L"Version Info \u263A")
+    .addOptionDouble('j', L"jared", L"j\u00C6red");
+    
     if(argc == 1) {
         argz.help(std::wcout);
         return 0;
