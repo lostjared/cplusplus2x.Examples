1cb2f374a8931b7224fa0ba266bb136b53be71c9
Jared Bruni <jaredbruni@gmail.com>
2024-03-23 01:34:13 -0700

    for_each

--- C++20/ranges/range.cpp
+++ C++20/ranges/range.cpp
@@ -1,10 +1,10 @@
+// as of Sat Mar 23 2024 only works on GCC not Clang
+
 #include<iostream>
 #include<algorithm>
 #include<string>
 #include<ranges>
 #include<vector>
-
-
 
 int main() {
     std::vector<std::string> v{"one ", "two", "apples", "bannas", "Turkey"};
@@ -12,5 +12,8 @@ int main() {
     for(const auto &i : std::views::take(v, 3)) {
         std::cout << i << "\n";
     }
+    std::ranges::for_each(v, [](auto s) {
+        std::cout << s << "\n";
+    });
     return 0;
 }
