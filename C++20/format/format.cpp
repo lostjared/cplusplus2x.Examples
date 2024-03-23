179e84ff4c9e334bc8b82dd71ee17543b432a63f
Jared Bruni <jaredbruni@gmail.com>
2024-03-23 11:15:17 -0700

    updated

---
+++ C++20/format/format.cpp
@@ -0,0 +1,15 @@
+/*
+ use g++-13 not support on Clang yet
+ $ CXX=g++-13 make
+*/
+
+#include<iostream>
+#include<format>
+
+int main(int argc, char **argv) {
+    std::cout << std::format("Hello, World {:d}\n", 10);
+    for(int i = 0; i < 10; ++i) {
+        std::cout << std::format("{:d} -> Hello Index\n", i);
+    }
+    return 0;
+}
