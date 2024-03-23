837c15255cd45008bcb1b2b4da791ae8f151d155
Jared Bruni <jaredbruni@gmail.com>
2024-03-23 01:50:17 -0700

    updated

--- C++20/ranges/range.cpp
+++ C++20/ranges/range.cpp
@@ -15,5 +15,14 @@ int main() {
     std::ranges::for_each(v, [](auto s) {
         std::cout << s << "\n";
     });
+    std::ranges::sort(v, [](auto v1, auto v2) {
+        return v2 < v1;
+    });
+    std::ranges::for_each(v, [](auto s) {
+        std::cout << "reverse: " << s << "\n";
+    });
+    for(const auto &i : std::views::all(v)) {
+        std::cout << "X: "<< i << "\n";
+    }
     return 0;
 }
