47cd8f67edc344f4529d5d6d00cc799fef78c69d
Jared Bruni <jaredbruni@gmail.com>
2024-03-23 20:58:35 -0700

    updated

--- C++20/constexpr/constexpr.cpp
+++ C++20/constexpr/constexpr.cpp
@@ -11,14 +11,13 @@ constexpr auto sum(const std::array<int, S> &a) {
 }
 
 int main() {
-    constexpr std::array<int, 4> values{0, 1, 2, 4};
+    constexpr static std::array<int, 4> values{0, 1, 2, 4};
     constexpr static auto index = sum(values);
     std::cout << "Caculated at compile time: " << index << "\n";
 }
 
-/* without constexpr static
-
-# Compilation provided by Compiler Explorer at https://godbolt.org/
+// Compiles to:
+/* 
 .LC0:
         .string "Caculated at compile time: "
 .LC1:
@@ -26,41 +25,6 @@ int main() {
 main:
         push    rbp
         mov     rbp, rsp
-        sub     rsp, 32
-        mov     DWORD PTR [rbp-32], 0
-        mov     DWORD PTR [rbp-28], 1
-        mov     DWORD PTR [rbp-24], 2
-        mov     DWORD PTR [rbp-20], 4
-        mov     QWORD PTR [rbp-8], 7
-        mov     esi, OFFSET FLAT:.LC0
-        mov     edi, OFFSET FLAT:_ZSt4cout
-        call    std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)
-        mov     rdx, rax
-        mov     rax, QWORD PTR [rbp-8]
-        mov     rsi, rax
-        mov     rdi, rdx
-        call    std::basic_ostream<char, std::char_traits<char> >::operator<<(unsigned long)
-        mov     esi, OFFSET FLAT:.LC1
-        mov     rdi, rax
-        call    std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)
-        mov     eax, 0
-        leave
-        ret
-*/
-/* with cosntexpr static 
-# Compilation provided by Compiler Explorer at https://godbolt.org/
-.LC0:
-        .string "Caculated at compile time: "
-.LC1:
-        .string "\n"
-main:
-        push    rbp
-        mov     rbp, rsp
-        sub     rsp, 16
-        mov     DWORD PTR [rbp-16], 0
-        mov     DWORD PTR [rbp-12], 1
-        mov     DWORD PTR [rbp-8], 2
-        mov     DWORD PTR [rbp-4], 4
         mov     esi, OFFSET FLAT:.LC0
         mov     edi, OFFSET FLAT:_ZSt4cout
         call    std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)
@@ -71,6 +35,6 @@ main:
         mov     rdi, rax
         call    std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)
         mov     eax, 0
-        leave
+        pop     rbp
         ret
 */
---
+++ C++20/constexpr/example2/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<array>
+#include<cstdio>
+
+template<size_t N>
+constexpr static const int find(const std::array<int, N> &values, const int item) {
+    for(size_t i = 0; i < values.size(); ++i) {
+        if(values[i] == item) 
+        return i;
+    }
+    constexpr const int not_found = -1;
+    return not_found;
+}
+
+int main() {
+    constexpr static std::array<int, 10> values{1,2,3,4,4,6,1,2,5,10};
+    constexpr static const int item = 5;
+    constexpr static int pos = find(values, item);
+    std::printf("indxx: %d\n", pos);
+    return 0;
+}
+
+// compiles to without optimizations
+
+/*
+.LC0:
+        .string "indxx: %d\n"
+main:
+        push    rbp
+        mov     rbp, rsp
+        mov     esi, 8
+        mov     edi, OFFSET FLAT:.LC0
+        mov     eax, 0
+        call    printf
+        mov     eax, 0
+        pop     rbp
+        ret
+*/
