a90e8a3acdbb87261f4515f0492a973ab1527c60
Jared Bruni <jaredbruni@gmail.com>
2024-03-26 15:32:46 -0700

    Lambda

--- C++20/lambda/lambda.cpp
+++ C++20/lambda/lambda.cpp
@@ -6,11 +6,18 @@ void Fill(F func) {
 	func(0);
 	func("test");
 	func(13.4);
+	func(25.2f);
 }
 
 int main() {
-	auto test = []<typename T>(const T &type) { std::cout "template typename: " << type << "\n"; };
+	auto test = []<typename T>(const T &type) { std::cout << "template typename: " << type << "\n"; };
 	Fill(test);
-	Fill([](const auto &t) { std::cout << "auto: " << t << "\n"; });
+	Fill([](const auto &t) {
+		if constexpr (std::is_same<decltype(t), const float &>::value) {
+			std::cout << "filtered out floating point: " << t << "\n";
+			return;
+		}
+		std::cout << "auto: " << t << "\n";
+	});
 	return 0;
 }
