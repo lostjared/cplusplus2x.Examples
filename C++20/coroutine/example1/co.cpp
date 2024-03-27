59983cc3a548319353605263f9adad899b1083f3
Jared Bruni <jaredbruni@gmail.com>
2024-03-27 13:16:45 -0700

    now uses template

--- C++20/coroutine/example1/co.cpp
+++ C++20/coroutine/example1/co.cpp
@@ -4,30 +4,32 @@
 #include <cstdlib>
 #include <iostream>
 #include<string>
+
+template<typename T>
 class Task {
 public:
 	template <typename Type>
 	struct Promise {
-		std::string value;
+		T value;
 		auto get_return_object() { return std::coroutine_handle<Promise<Type>>::from_promise(*this); }
 		auto initial_suspend() { return std::suspend_always{}; }
 		auto final_suspend() noexcept { return std::suspend_always{}; }
 		void unhandled_exception() { std::terminate(); }
 		void return_void() {}
-		std::suspend_always yield_value(std::string val) {
+		std::suspend_always yield_value(T val) {
 			value = val;
 			return {};
 		}
 	};
-	using promise_type = Promise<Task>;
+	using promise_type = Promise<Task<T>>;
 	Task(auto h) : coro{h} {}
 	~Task() {
 		if (coro) {
 			coro.destroy();
 		}
 	}
-	Task(const Task &) = delete;
-	Task &operator=(const Task &) = delete;
+	Task(const Task<T> &) = delete;
+	Task<T> &operator=(const Task <T>&) = delete;
 
 	bool resume() const {
 		if (!coro) {
@@ -36,28 +38,27 @@ public:
 		coro.resume();
 		return !coro.done();
 	}
-
-	std::string getValue() { return coro.promise().value; }
-
+	T getValue() { return coro.promise().value; }
 private:
 	std::coroutine_handle<promise_type> coro;
 };
 
 // coroutines would work perfect for a text scanner
+template<typename T>
 class Scanner {
 public:
-    Task getNext() {
+    Task<T> getNext() {
         while(cur_token.length() < 5) {
             cur_token += "x";
             co_yield cur_token;
         }
     }
 private:
-    std::string cur_token;
+    T cur_token;
 };
 
 int main() {
-    Scanner scan;
+    Scanner<std::string> scan;
 	auto i = scan.getNext();
 	while (i.resume()) {
 		std::cout << "call -> " << i.getValue() << "\n";
