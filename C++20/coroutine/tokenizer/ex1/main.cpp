4fd61a9f16770e157579e128d38430aec3790ba0
Jared Bruni <jaredbruni@gmail.com>
2024-03-28 02:00:21 -0700

    brace initalizer/formatting

--- C++20/coroutine/tokenizer/ex1/main.cpp
+++ C++20/coroutine/tokenizer/ex1/main.cpp
@@ -1,27 +1,27 @@
 /* use tokenizer with wchar_t */
 
-#include"../tokenizer.hpp"
-#include<iostream>
-#include<string>
-#include<algorithm>
-#include<ranges>
+#include "../tokenizer.hpp"
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <string>
 
 int main(int argc, char **argv) {
-    Scanner<std::wstring> scanner;
-    bool active = true;
-    while(active) {
-        std::wcout << L"Enter a sentence..\n";
-        std::wstring s;
-        std::getline(std::wcin, s);
-        scanner.set(s, L" ");
-        auto tokens{collect(scanner.tokenizer())};
-        int index = 0;
-        for(const auto &token : std::views::all(tokens)) {
-            std::wcout << L"token: " << index++ << L" " << token << L"\n";
-        }
-        if(tokens.size() > 0 && tokens.at(0) == L"quit") {
-            active = false;
-        }
-    }
-    return 0;
+	Scanner<std::wstring> scanner;
+	bool active{true};
+	while (active) {
+		std::wcout << L"Enter a sentence..\n";
+		std::wstring s;
+		std::getline(std::wcin, s);
+		scanner.set(s, L" ");
+		auto tokens{collect(scanner.tokenizer())};
+		int index = 0;
+		for (const auto &token : std::views::all(tokens)) {
+			std::wcout << L"token: " << index++ << L" " << token << L"\n";
+		}
+		if (tokens.size() > 0 && tokens.at(0) == L"quit") {
+			active = false;
+		}
+	}
+	return 0;
 }
