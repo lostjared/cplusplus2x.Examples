ee516972cc12c973ccbb6bf86c959e0da8d40546
Jared Bruni <jaredbruni@gmail.com>
2024-03-28 16:17:28 -0700

    id

--- C++20/jthread/ex2/t.cpp
+++ C++20/jthread/ex2/t.cpp
@@ -7,13 +7,13 @@
 void thread1(std::stop_token token) {
     auto id = std::this_thread::get_id();
     std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "finished here..\n";
+    std::cout << id << " -> finished here..\n";
 }
 
 
 int main() {
     std::jthread t(thread1);
-    std::cout << "sleep 3 seconds while thread1 proc\n";
+    std::cout << "sleep 3 seconds while thread1: " << t.get_id() << " proc\n";
     std::this_thread::sleep_for(std::chrono::seconds(3));
     // destructor will join
     return 0;
