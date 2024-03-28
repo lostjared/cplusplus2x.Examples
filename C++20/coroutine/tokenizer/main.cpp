9375fcf15772aa7df1f7fafd5734bb16ffb42c52
Jared Bruni <jaredbruni@gmail.com>
2024-03-28 01:30:06 -0700

    broken into header/source

---
+++ C++20/coroutine/tokenizer/main.cpp
@@ -0,0 +1,32 @@
+/* Use GCC 13.2  or greater */
+// Help from Chapter 15 C++ 20 The Complete Guide
+#include "tokenizer.hpp"
+
+int main() {
+	Scanner<std::string> scan("test one two three", " ");
+	auto i{scan.tokenizer()};
+	int index = 0;
+	while (i.resume()) {
+		++index;
+		std::cout << "calling index: " << index << " -> " << i.getValue() << "\n";
+	}
+	scan.set("test::cpp::plus", "::");
+	auto s{scan.tokenizer()};
+	index = 0;
+	while (s.resume()) {
+		++index;
+		std::cout << "index: " << index << " -> " << s.getValue() << "\n";
+	}
+	scan.set("test;one;two;three;;;", ";");
+	auto v{collect(scan.tokenizer())};
+	for (const auto &i : std::views::all(v)) {
+		std::cout << "vector: " << i << "\n";
+	}
+	scan.set("abc->one->two->apples", "->");
+	auto t{scan.tokenizer()};
+	auto v2{collect(std::move(t))};
+	for (const auto &i : std::views::all(v2)) {
+		std::cout << i << "\n";
+	}
+	return 0;
+}
