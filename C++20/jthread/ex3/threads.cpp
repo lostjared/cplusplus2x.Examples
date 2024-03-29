0f1f3d5d6c507a615508ae2acb1a76a43d852f7d
Jared Bruni <jaredbruni@gmail.com>
2024-03-29 02:17:37 -0700

    ex3

---
+++ C++20/jthread/ex3/threads.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<thread>
+#include<vector>
+#include<mutex>
+#include<stop_token>
+
+std::mutex mut_lock;
+
+void thread_test(std::stop_token token, int id) {
+    while(!token.stop_requested()) {
+        mut_lock.lock();
+        std::cout << "id: " << id << "-> echo..\n";
+        mut_lock.unlock();
+    }
+    std::cout << "thread: " << id << " done!\n";
+}
+
+
+int main() {
+
+    std::vector<std::jthread> t_proc;
+    for(int i = 0;  i < 5; ++i) {
+        t_proc.push_back(std::jthread(thread_test, i));
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    for(auto &t : t_proc) {
+        t.request_stop();
+    }
+    std::cout << "join. on destruct...\n";
+}
