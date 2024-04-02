e1faa9f0cde71d7d6cd3553f01a57543189eb1d3
Jared Bruni <jaredbruni@gmail.com>
2024-04-02 05:39:58 -0700

    module-test

---
+++ C++20/modules/main.cpp
@@ -0,0 +1,10 @@
+/* compile with GCC 13.2 */
+
+import math;
+#include <iostream>
+
+int main() {
+    std::cout << "Sum: " << add(1, 2) << "\n";
+    std::cout << "Product: " << mul(1, 2) << "\n";
+    return 0;
+}
