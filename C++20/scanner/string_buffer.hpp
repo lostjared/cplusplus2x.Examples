cfacbf0c0a7133767c6e9a985efcd69e0db33eeb
Jared Bruni <jaredbruni@gmail.com>
2024-08-16 11:49:00 -0700

    Update string_buffer.hpp

--- C++20/scanner/string_buffer.hpp
+++ C++20/scanner/string_buffer.hpp
@@ -7,13 +7,11 @@
 #include<optional>
 
 namespace scan {
-
     namespace buffer {
         class StringBuffer {
         public:
             using ch_type = int8_t;
             using string_type = std::basic_string<ch_type, std::char_traits<ch_type>>;
-
             StringBuffer(const string_type &buf) : buffer_{buf}, index{0} {}
             StringBuffer(const StringBuffer &sb) :  buffer_{sb.buffer_}, index{0} {}   
             StringBuffer(StringBuffer &&sb) :  buffer_{std::move(sb.buffer_)}, index{0} {}
@@ -26,6 +24,7 @@ namespace scan {
             std::optional<ch_type> peekch();
 
             bool eof(uint64_t pos);
+            void reset(uint64_t pos = 0);
 
             private:
                string_type buffer_;           
@@ -33,6 +32,22 @@ namespace scan {
         };
     }
 
+    namespace token {
+        enum class TokenType { TT_ID, TT_SYM, TT_STR, TT_NULL };
+        class Token {
+        public:
+            using ch_type = int8_t;
+            using string_type = std::basic_string<ch_type, std::char_traits<ch_type>>;
+            Token() = default;
+            Token(const TokenType &type);
+            void setToken(const TokenType &type, const string_type &value);
+            string_type getValue() const { return value; }
+            TokenType getType() const { return type; }
+        private:
+            TokenType type;
+            string_type value;
+        };
+    }
 }
 
 
