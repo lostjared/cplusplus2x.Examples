606125a9eba61fc7379d4d2bf8bd182d528c5eff
Jared Bruni <jaredbruni@gmail.com>
2024-08-19 11:53:12 -0700

    Added keywords to HTML generator
    Keywords

--- C++20/scanner/html.cpp
+++ C++20/scanner/html.cpp
@@ -3,6 +3,8 @@
 #include<sstream>
 #include<iostream>
 #include<memory>
+#include<set>
+#include<string>
 
 std::string htmlPage_Header = R"(
 <!DOCTYPE html>
@@ -53,6 +55,35 @@ std::string convertToHTML(const std::string &text) {
     return stream.str();
 }
 
+std::set<std::string> createKeywordSet() {
+    return {
+        // C++ Keywords
+        "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit", "atomic_noexcept",
+        "auto", "bitand", "bitor", "bool", "break", "case", "catch", "char", "char8_t", "char16_t", "char32_t",
+        "class", "compl", "concept", "const", "consteval", "constexpr", "constinit", "const_cast", "continue",
+        "co_await", "co_return", "co_yield", "decltype", "default", "delete", "do", "double", "dynamic_cast",
+        "else", "enum", "explicit", "export", "extern", "false", "float", "for", "friend", "goto", "if", "inline",
+        "int", "long", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or",
+        "or_eq", "private", "protected", "public", "reflexpr", "register", "reinterpret_cast", "requires", "return",
+        "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "synchronized",
+        "template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename", "union",
+        "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+
+        // JavaScript Keywords
+        "await", "debugger", "delete", "export", "extends", "finally", "function", "import", "in", "instanceof", 
+        "let", "new", "null", "super", "this", "throw", "typeof", "var", "void", "with", "yield",
+
+        // C Keywords (remaining after duplicates removed)
+        "restrict", "_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Decimal128", "_Decimal32", "_Decimal64",
+        "_Generic", "_Imaginary", "_Noreturn", "_Static_assert", "_Thread_local"
+    };
+}
+
+bool isKeyword(const std::string& word) {
+    static std::set<std::string> keywordSet = createKeywordSet();
+    return keywordSet.find(word) != keywordSet.end();
+}
+
 int html_scanFile(const std::string &contents, const char *out_filename) {
     try {
         std::fstream file;
@@ -71,8 +102,14 @@ int html_scanFile(const std::string &contents, const char *out_filename) {
 
         for(size_t i = 0; i < scan->size(); ++i) {
             auto posx = scan->operator[](i).get_pos();
-            file << "<tr><td>" << i << "/" << posx.first << ":" << posx.second << "</td><td>" << convertToHTML(scan->operator[](i).getTokenValue()) << "</td><td>";
-            types::print_type_TokenType(file, scan->operator[](i).getTokenType());
+            file << "<tr><td>" << i << "/" << posx.first << ":" << posx.second << "</td><td>";
+            if(!isKeyword(scan->operator[](i).getTokenValue())) {
+                file << convertToHTML(scan->operator[](i).getTokenValue()) << "</td><td>";
+                types::print_type_TokenType(file, scan->operator[](i).getTokenType());
+            } else {
+                file << "<b>" << convertToHTML(scan->operator[](i).getTokenValue()) << "</b>" << "</td><td>";
+                file << "Keyword";
+            }
             file << "</td></tr>\n";
         }
         file << htmlPage_Footer << "\n";
