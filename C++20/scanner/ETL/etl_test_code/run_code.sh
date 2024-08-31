08e8ac05fde8a2f498f7c01019ff6b0a38e3885a
Jared Bruni <jaredbruni@gmail.com>
2024-08-31 01:13:29 -0700

    unit test

---
+++ C++20/scanner/ETL/etl_test_code/run_code.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+directories=("addnumber" "c_code" "cpp_code" "hello" "macros" "number" "obj" "str" "strings")
+
+for dir in "${directories[@]}"; do
+    echo "Entering directory: $dir"
+    cd "$dir"
+    
+    if make > stdout.txt 2>&1; then
+        echo "Compilation successful in $dir"
+	echo "Executing..."
+        if ./program ; then
+		echo "Success.."
+	else
+		echo "Failure program crashed"
+		exit 1
+	fi
+    else
+        echo "Compilation failed in $dir. Check stdout.txt for details."
+        cd ..
+        exit 1
+    fi
+    
+    cd ..
+done
+
+echo "All compilations completed successfully."
