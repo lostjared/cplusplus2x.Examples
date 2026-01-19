2f0a748d16b3fd59740be76e8069bb545207dd61
Jared Bruni <jaredbruni@gmail.com>
2026-01-19 07:08:56 -0800

    updated example

--- C++20/semaphore/semaphore.cpp
+++ C++20/semaphore/semaphore.cpp
@@ -1,25 +1,26 @@
-#include <iostream>
-#include <thread>
-#include <semaphore>
-#include <string>
+#include<iostream>
+#include<thread>
+#include<semaphore>
+#include<string>
+#include<syncstream>
 
 std::binary_semaphore waiter(0);
+std::string input_data;
 
 void worker_thread() {
-    std::cout << "   [Thread] Launched. I am now waiting for the signal...\n";
-    waiter.acquire();
-    std::cout << "   [Thread] Signal received! I am awake and working.\n";
+   std::osyncstream(std::cout) << "   [Thread] Launched. I am now waiting for the signal...\n";
+   waiter.acquire();
+   std::osyncstream(std::cout) << "   [Thread] Signal received! I am awake and working.\n";
+   std::osyncstream(std::cout) << "   Data entered: " << input_data << "\n";
 }
 
 int main() {
     std::thread t(worker_thread);
-    std::cout << "[Main] Thread is waiting. Press ENTER to wake it up...\n";
-    std::cout << "enter tex: ";
-    std::string line;
-    std::getline(std::cin, line);
-    std::cout << "[Main] Input received. Releasing semaphore...\n";
+    std::osyncstream(std::cout) << "[Main] Thread is waiting. Press ENTER to wake it up...\n";
+    std::getline(std::cin, input_data);
+    std::osyncstream(std::cout) << "[Main] Input received. Releasing semaphore...\n";
     waiter.release();
     t.join();
-    std::cout << "[Main] Done.\n";
+    std::osyncstream(std::cout) << "[Main] Done.\n";
     return 0;
 }
