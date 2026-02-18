fb9e56c2c78719a7e38e0c5d0a9d549928f867ea
Jared Bruni <jaredbruni@gmail.com>
2026-02-18 02:59:18 -0800

    updated wordsplit with shuffled version

---
+++ C++23/wordsplit/shuffle/uniq/main_shuffle.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<unordered_map>
+#include<cstdlib>
+#include<random>
+#include<ctime>
+#include<print>
+#include<algorithm>
+#include<unordered_set>
+
+std::string reverse_string(const std::string &);
+std::string shuffle_string(const std::string &);
+
+template<typename T>
+void echo_words(T &m) {
+    for(auto &i : m) {
+        std::print("{} ", shuffle_string(i));
+    }
+    std::print("\n");
+}
+
+std::string reverse_string(const std::string &text) {
+	std::string data;
+	for(int i = text.length() -1; i >= 0; --i) {
+		data += text[i];
+	}
+	return data;
+}
+
+std::string shuffle_string(const std::string &text) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    
+    std::string data;
+    if(text.length() >= 3) {
+        char first, last;
+        first = text[0];
+        last =  text[text.length()-1];
+        std::vector<char> ch;
+        for(size_t i = 1; i < text.length()-1; ++i) {
+            ch.push_back(text[i]);
+        }
+        std::shuffle(ch.begin(), ch.end(), gen);
+        data += first;
+        for(size_t i = 0; i < ch.size(); ++i) {
+            data += ch[i];
+        }
+        data += last;
+    } else {
+        data = text;
+    }
+    return data;
+}
+
+int main(int argc, char **argv) {
+
+    if(argc != 2) {
+        std::println("invalid args.");
+        return EXIT_FAILURE;
+    }
+
+    std::fstream file;
+    file.open(argv[1], std::ios::in);
+    if(!file.is_open()) {
+        std::println("Error: file not found/could not open: {}", argv[1]);
+        return EXIT_FAILURE;
+    }
+    std::ostringstream stream;
+    stream << file.rdbuf();
+    std::unordered_set<std::string> words;
+    size_t i = 0;
+    std::string s = stream.str();
+    std::string word;
+    size_t index = 0;
+
+    while (i < s.length()) {
+        if((s[i] >= 'a' && s[i] && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+            word += s[i];
+            ++i;
+            continue;
+        } else {
+            ++i;
+            if(!word.empty())
+                words.insert(word);
+            word = "";
+            continue;
+        }
+        ++i;
+    }
+    std::vector<std::string> shuffled;
+    shuffled.resize(words.size());
+    std::copy(words.begin(), words.end(), shuffled.begin());
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::shuffle(shuffled.begin(), shuffled.end(), gen);
+    echo_words(shuffled);
+    return 0;
+}
